a143f7d92af6206fa010807d8a1b470bb6c58e14
pbc1001 <mintcho0328@gmail.com>
2025-12-23 19:06:46 +0900

    fix: 로그인 & 공지사항 등록 해결

--- src/api/post.api.js
+++ src/api/post.api.js
@@ -1,14 +1,10 @@
-
 import publicApi from "./instance"; 
 
-/**
- * 공지사항 등록 API
- * @param {object} payload
- */
+
 export const createNotice = async (payload) => {
   try {
     const response = await publicApi.post(
-     "/write-notice",
+     "/post/write-notice",
      payload
 
     );
@@ -18,3 +14,56 @@ export const createNotice = async (payload) => {
     throw err;
   }
 };
+
+
+export const updateNotice = async (postId, payload) => {
+  try {
+    const response = await publicApi.put(
+     `/write-notice/${postId}`,
+      payload
+    );
+    return response.data;
+  } catch (err) {
+    console.error(`공지사항 수정 실패:`, err.response?.data || err.message);
+    throw err;
+  }
+};
+
+export const deletePost = async (postId) => {
+  try {
+    const response = await publicApi.delete(
+        `/write-notice/${postId}`
+    );
+    return response.data;
+  } catch (err) {
+    console.error(`게시물 삭제 실패:`, err.response?.data || err.message);
+    throw err;
+  }
+};  
+
+// export const createLost = async (payload) => {
+//   try {
+//     const response = await publicApi.post(
+//      "/write-lost",
+//      payload
+
+//     );
+//     return response.data;
+//   } catch (err) {
+//     console.error("공지사항 등록 실패:", err.response?.data || err.message);
+//     throw err;
+//   }
+// };
+
+// export const updateLost = async (postId, payload) => {
+//   try {
+//     const response = await publicApi.put(
+//      `/write-lost/${postId}`,
+//       payload
+//     );
+//     return response.data;
+//   } catch (err) {
+//     console.error(`분실물 게시글 수정 실패:`, err.response?.data || err.message);
+//     throw err;
+//   }
+// };
